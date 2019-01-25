73c213fb01fff5cf4e59735999b2aa5409fd1a50
Kiiskinen Mauno TTK17SP <maunokiiskinen@kamk.fi>
2019-01-25 21:23:58 +0200

    Folders and enemy spawner

---
+++ FGJ19/Assets/Scripts/MaunonScripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public Transform[] spawnPositions;
+    public float timer;
+
+    float timeBetweenSpawns = 5f;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            SpawnEnemy();
+            timer = timeBetweenSpawns;
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        int index = UnityEngine.Random.Range(0, spawnPositions.Length);
+        Vector3 spawnPosition = spawnPositions[index].transform.position;
+        GameObject tempEnemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+        tempEnemy.transform.parent = transform;
+    }
+}

6dc6a2bae9e57b5e661e15c98df661036929ab6c
LauriAue <31925904+LauriAue@users.noreply.github.com>
2019-01-26 00:08:41 +0200

    kaikkee turhaa ja pelaaja + DonkeyShrekt script

---
+++ FGJ19/Assets/Scripts/PurkkaScripts/DonkeyShrekt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DonkeyShrekt : MonoBehaviour
+{
+    [SerializeField] float power;
+    [SerializeField] float radius;
+    [SerializeField] float upforce;
+
+    [SerializeField] bool canhit;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canhit = true;
+            if (canhit && Input.GetMouseButtonDown(0))
+            {
+                SkyIsTheLimit();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canhit = false;
+        }
+    }
+
+    void SkyIsTheLimit()
+    {
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, upforce, ForceMode.Impulse);
+        }
+    }
+}

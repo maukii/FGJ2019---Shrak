b8dd09ca5e5ac56545cbf3c23dbfab46b409b9a6
LauriAue <31925904+LauriAue@users.noreply.github.com>
2019-01-27 00:50:47 +0200

    updates

--- FGJ19/Assets/Scripts/MaunonScripts/EnemySpawner.cs
+++ FGJ19/Assets/Scripts/MaunonScripts/EnemySpawner.cs
@@ -2,20 +2,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class EnemySpawner : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
     public Transform[] spawnPositions;
-    public float timeBetweenSpawns = 5f;
+    public float timeBetweenSpawns = 6f;
     public Transform target;
+    public Text timerText;
+    private float time = 180f;
 
     float timer;
 
+    void Start()
+    {
+        StartCoundownTimer();
+    }
+
+    void StartCoundownTimer()
+    {
+        if (timerText != null)
+        {
+            time = 180f;
+            timerText.text = "Survive: 03:00";
+            InvokeRepeating("UpdateTimer", 0.0f, 0.01667f);
+        }
+    }
+
+    void UpdateTimer()
+    {
+        if (timerText != null)
+        {
+            time -= Time.deltaTime;
+            string minutes = Mathf.Floor(time / 60).ToString("00");
+            string seconds = (time % 60).ToString("00");
+            timerText.text = "Survive: " + minutes + ":" + seconds;
+        }
+    }
+
+    //time and timer, what could go wrong?
     private void Update()
     {
         timer -= Time.deltaTime;
+
+        if (time < 140)
+        {
+            timeBetweenSpawns = 5f;
+        }
+
+        if (time < 100)
+        {
+            timeBetweenSpawns = 4f;
+        }
+
+        if (time < 60)
+        {
+            timeBetweenSpawns = 3f;
+        }
+
+        if (time < 20)
+        {
+            timeBetweenSpawns = 2f;
+        }
 
         if(timer <= 0)
         {
@@ -30,5 +81,22 @@ public class EnemySpawner : MonoBehaviour
         Vector3 spawnPosition = spawnPositions[index].transform.position;
         GameObject tempEnemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
         tempEnemy.transform.parent = transform;
+
+        if (time < 140)
+        {
+            tempEnemy.GetComponent<NavMeshAgent>().speed = 6f;
+        }
+        if (time < 100)
+        {
+            tempEnemy.GetComponent<NavMeshAgent>().speed = 7f;
+        }
+        if (time < 60)
+        {
+            tempEnemy.GetComponent<NavMeshAgent>().speed = 8f;
+        }
+        if (time < 20)
+        {
+            tempEnemy.GetComponent<NavMeshAgent>().speed = 10f;
+        }
     }
 }

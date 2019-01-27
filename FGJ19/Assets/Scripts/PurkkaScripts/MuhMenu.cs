98b4c8ec429e03293ee1be3c652247610767fc60
LauriAue <31925904+LauriAue@users.noreply.github.com>
2019-01-27 04:18:20 +0200

    next-gen-menu

---
+++ FGJ19/Assets/Scripts/PurkkaScripts/MuhMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuhMenu : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void CreSwitch()
+    {
+        anim.SetTrigger("doorOpen");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
